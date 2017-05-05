e84a7f23fcaf8e2b9848bf83bb731ea842ee66bd
Yutaka Tsutano <yutaka@tsutano.com>
2017-05-05 03:50:47 -0500

    axmldec 1.1.0

--- axmldec.rb
+++ axmldec.rb
@@ -1,9 +1,9 @@
 class Axmldec < Formula
   desc "Stand-alone binary AndroidManifest.xml decoder"
   homepage "https://github.com/ytsutano/axmldec"
-  url "https://github.com/ytsutano/axmldec/archive/v1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "556590e00bd36b3ede9ced4d4793429a874fb382e35ac80ffcc621a6574a7b80"
+  url "https://github.com/ytsutano/axmldec/archive/v1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 "41875f31f83a96788b130663774d07ca50460cabc5e32ef054d1296f73ec30da"
 
   depends_on "cmake" => :build
   depends_on "boost" => "c++11"
